3bc689fbd9ed22a82aacb529cd481643fa8385fb
mkonstantinov <N29V8gw8NwhI8vwI>
2018-08-07 12:25:51 +0300

    .

---
+++ src/main/scala/Utils.scala
@@ -0,0 +1,28 @@
+import java.sql.Timestamp
+
+import StreamingApp.log
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import play.api.libs.json.Json
+
+import scala.util.{Failure, Success, Try}
+
+trait Utils {
+
+  @transient private lazy val log = LoggerFactory.getLogger(this.getClass)
+
+  def jsonString2Event(jsonString: String): Option[Event] = Try({
+    val json = Json.parse(jsonString)
+    val categoryId = (json \ "category_id").get.as[Int]
+    val timestamp = new Timestamp((json \ "unix_time").get.as[Long])
+    val ip = (json \ "ip").get.as[String]
+    val eventType = (json \ "type").get.as[String]
+    Event(ip, timestamp, categoryId, eventType)
+  }) match {
+    case Success(x) => Some(x)
+    case Failure(f) =>
+      log.warn(s"Couldn't parse a row: $jsonString", f)
+      None
+  }
+
+}
